a0fd0aeac1a03a7a3dab38868aea14c978aa0dc3
Kelly Rhodes <krhodes1@fullsail.edu>
2016-03-25 09:59:11 -0400

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// gulpfile ******************************************
+
+// Dependencies
+var gulp  = require('gulp');
+var sass = require('gulp-ruby-sass') ;
+var notify = require("gulp-notify") ;
+var bower = require('gulp-bower');
+
+// Config
+var config = {
+	bowerDir: './bower_components'
+}
+
+// Gulp Tasks
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+gulp.task('icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('./public/fonts')); 
+});
+
+gulp.task('css', function() { 
+    return sass('sass/style.scss', {
+             style: 'compressed',
+             loadPath: [
+                 './sass',
+                 config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+                 config.bowerDir + '/font-awesome/scss',
+             ]
+         }) 
+         .pipe(gulp.dest('./public/css')); 
+});
+
+
+// Rerun the task when a file changes
+ gulp.task('watch', function() {
+     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+});
+
+  gulp.task('default', ['bower', 'icons', 'css']);
